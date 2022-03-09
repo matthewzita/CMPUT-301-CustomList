aad30edaf7103ee70d7af8e0b3739d2b787b6cf8
matzit20 <54906052+mzita@users.noreply.github.com>
2022-03-09 15:55:03 -0700

    failing deleteCity

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -87,7 +87,7 @@ public class CustomList extends ArrayAdapter<City> {
         if(!cities.contains(city)) {
             throw new IllegalArgumentException();
         }
-        cities.remove(city);
+//        cities.remove(city);
 
     }
 
